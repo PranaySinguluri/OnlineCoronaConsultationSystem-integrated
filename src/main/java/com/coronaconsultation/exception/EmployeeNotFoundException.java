eb897206f8ac46b4c098ccd7e86e9f96857fc761
PranaySinguluri <84218913+PranaySinguluri@users.noreply.github.com>
2024-04-30 13:43:25 -0500

    chnages made in exceptions

--- src/main/java/com/coronaconsultation/exception/EmployeeNotFoundException.java
+++ src/main/java/com/coronaconsultation/exception/EmployeeNotFoundException.java
@@ -3,9 +3,9 @@ package com.coronaconsultation.exception;
 import java.sql.SQLException;
 
 public class EmployeeNotFoundException  extends RuntimeException{
+
+	String message =" No Employee Found";
 	public EmployeeNotFoundException(String message) {
 		super(message);	
 	}
-	
-
 }
--- src/main/java/com/coronaconsultation/exception/MedicalReportNotFoundException.java
+++ src/main/java/com/coronaconsultation/exception/MedicalReportNotFoundException.java
@@ -1,6 +1,8 @@
 package com.coronaconsultation.exception;
 
 public class MedicalReportNotFoundException extends RuntimeException {
+
+	String message ="not found";
 public MedicalReportNotFoundException(String message) {
 	super(message);
 }
