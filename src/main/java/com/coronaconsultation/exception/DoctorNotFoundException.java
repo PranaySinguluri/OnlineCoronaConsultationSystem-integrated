ad3b4dc7f27e99ff39c5a1eeeb0fd1de4bd9e2f2
PranaySinguluri <84218913+PranaySinguluri@users.noreply.github.com>
2024-04-30 11:12:00 -0500

    added message to DoctorNotFoundException

--- src/main/java/com/coronaconsultation/exception/DoctorNotFoundException.java
+++ src/main/java/com/coronaconsultation/exception/DoctorNotFoundException.java
@@ -3,6 +3,7 @@ package com.coronaconsultation.exception;
 import java.sql.SQLException;
 
 public class DoctorNotFoundException extends RuntimeException {
+	String message = " No doctor found";
 	public DoctorNotFoundException(String message) {
 		super(message);
 	}
