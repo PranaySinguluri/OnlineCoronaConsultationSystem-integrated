eb60bc1866cf277a169d2c0f49ad4d504934f3bb
pranay singuluri <singuluripranay20.com>
2026-02-28 20:51:14 -0600

    added home controller and spring security

--- src/main/java/com/coronaconsultation/controller/HomeController.java
+++ src/main/java/com/coronaconsultation/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.coronaconsultation.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "welcome";
+    public String home(HttpSession session) {
+
+        return "welcome " + session.getId();
     }
 }
